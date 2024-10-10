e4a8405ba76e2debf4eb7452ada6e602cafc626e
甜力怕 <im@932.moe>
2024-10-10 09:45:45 +0800

    v0.0.2

--- readme.md
+++ readme.md
@@ -2,4 +2,12 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-zanwo?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-zanwo)
 
-onebot 平台赞我插件
+> QQ 名片赞点赞插件
+
+## 贡献指南
+
+欢迎大家使用这些插件，并提出问题反馈，我会在有时间的时候修复它。
+
+## 鸣谢
+
+本插件基于 JetBrains 相关工具及软件构建，感谢 JetBrains 对开源项目的大力支持。本插件部分文档使用 CEC-IDE 编写。
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 
 export const name = 'zanwo'
 
-export interface Config {}
+export const usage = `
+**<center>本插件开箱即用</center>**
+
+常见的失败原因：用户禁止陌生人赞我、今日点赞已达上限次数或与服务器通讯出错
+
+本插件支持陌生人点赞 50 次 及 自定义成功失败语句
+`;
+
+
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.i18n.define('zh', require('./locales/zh'))
+
+  ctx.command('zanwo').alias('赞我').action(async ({ session }) => {
+    let num = 0
+    try {
+      for (let i = 0; i < 5; i++) {
+        await session.bot.internal.sendLike(session.userId, 10);
+      }
+      return session.send(h('quote', { id: session.messageId }) + session.text('.success'));
+    }
+    catch (_e) {
+      if (num > 0) return session.send(h('quote', { id: session.messageId }) + session.text('.success'));
+      return session.send(h('quote', { id: session.messageId }) + session.text('.failure'));
+    }
+  });
 }
